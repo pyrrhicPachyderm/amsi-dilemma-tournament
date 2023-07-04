760d47a003b16cdf2491a57adaaedd00098ce354
Christopher Brown <pyrrhicpachyderm@gmail.com>
2023-07-04 22:25:40 +1000

    Add the Always Cooperate and Always Defect textbook strategies.

---
+++ strategies/always_cooperate.py
@@ -0,0 +1,5 @@
+name = "Always Cooperate"
+owner = "Textbook"
+
+def play(p1_moves, p2_moves, T, R, P, S, delta):
+	return True
---
+++ strategies/always_defect.py
@@ -0,0 +1,5 @@
+name = "Always Defect"
+owner = "Textbook"
+
+def play(p1_moves, p2_moves, T, R, P, S, delta):
+	return False
