281fabf1c05080769436a8d4adf7f445a61b761e
Christopher Brown <pyrrhicpachyderm@gmail.com>
2023-07-04 20:26:56 +1000

    Add a safe-round function to the repeat module.
    For rounding while preserving the sum.

--- utils/repeat.py
+++ utils/repeat.py
@@ -1,6 +1,23 @@
+from math import floor
+from operator import itemgetter
+
 #Takes two lists of equal length, and returns a list with each element of x repeated the corresponding number of times.
 def rep(x, times):
 	result = []
 	for val, n in zip(x, times):
 		result.extend([val] * n)
 	return result
+
+#Takes a list of floating point numbers, and rounds the list to integers while preserving the sum.
+def safe_round(xs):
+	total = round(sum(xs))
+	#First, we round everything down.
+	results = [floor(x) for x in xs]
+	#Then, we round up the numbers with the largest fractional parts until the sum is correct.
+	residuals = [x%1 for x in xs]
+	residuals = sorted(enumerate(residuals), key = itemgetter(1), reverse = True) #Sort descending by values.
+	difference = total - sum(results)
+	for i in range(difference):
+		results[residuals[i][0]] += 1
+	
+	return results
