bd65df990c4cc8a1130109ef6577aee0124df427
Christopher Brown <pyrrhicpachyderm@gmail.com>
2023-07-04 15:25:53 +1000

    Add parameters for the evolutionary tournament to the parameters file.

--- utils/parameters.py
+++ utils/parameters.py
@@ -1,5 +1,10 @@
+#Parameters for iterated prisoner's dilemma
 T = 5
 R = 3
 P = 1
 S = 0
 delta = 0.99
+
+#Parameters for the evolutionary tournament
+N_per = 10 #Number of starting individuals of each strategy
+num_gen = 100 #Number of generations
