872eddcf1dbc8728ff047312e9cef1d51e62d95c
Christopher Brown <pyrrhicpachyderm@gmail.com>
2023-07-04 18:07:18 +1000

    Rewrite rep() to use zip() to be more Pythonic.

--- utils/repeat.py
+++ utils/repeat.py
@@ -1,6 +1,6 @@
 #Takes two lists of equal length, and returns a list with each element of x repeated the corresponding number of times.
 def rep(x, times):
 	result = []
-	for i in range(len(x)):
-		result.extend([x[i]] * times[i])
+	for val, n in zip(x, times):
+		result.extend([val] * n)
 	return result
