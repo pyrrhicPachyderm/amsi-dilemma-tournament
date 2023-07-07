ec41c317c14eb31dfb44b2f222fc6d3cff0089c1
Madhav Padmakumar <35822368+MadhavPadmakumar@users.noreply.github.com>
2023-07-07 11:31:06 +1000

    Added hungry hungry hippo strategy.

---
+++ strategies/hungry_hungry_hippo.py
@@ -0,0 +1,25 @@
+name = "Hungry Hungry Hippo"
+owner = "Madhav"
+
+def play(p1_moves, p2_moves, T, R, P, S, delta):
+	# Testing phase: D C C
+	if len(p2_moves) == 0:
+		return False
+	elif len(p2_moves) == 1:
+		return True
+	elif len(p2_moves) == 2:
+		return True
+	# Response phase: Diagnose opponent as one of the following.
+	# Rude: Turn 1 defect
+	# Weak: No defects
+	# Nice: Turn 2 D, turn 3 C
+	# Angy: Turn 3 D
+	
+	# Then perform the following response strategies:
+	# Rude || Weak || Angy: Always defect.
+	# Nice: Always C.
+	elif(not p2_moves[0] or all(p2_moves) or not p2_moves[2]):
+		return False
+	else:
+		return True
+	
