f942f92272686d8ea530c7dd5d9873004979b179
Christopher Brown <pyrrhicpachyderm@gmail.com>
2023-07-13 11:15:06 +1000

    Change game length to be determined up front using nbinom.

--- utils/game.py
+++ utils/game.py
@@ -1,4 +1,5 @@
 import random
+from scipy.stats import nbinom
 import utils.parameters as parameters
 
 def play(strategy, p1_moves, p2_moves):
@@ -18,16 +19,21 @@ def payoff(p1_move, p2_move):
 		else:
 			return parameters.P
 
+def roll_game_length():
+	#NegativeBinomial(r, p) is the number of failures before r successes.
+	#So 1 + NegativeBinomial(1, 1 - delta) is the number of games.
+	return 1 + nbinom.rvs(1, 1 - parameters.delta)
+
 #Takes two strategies, and runs a game.
 #Returns the total payoff for both strategies.
 #If normalise = True, results are divided by the number of iterations, to control for variable game length.
 def game(strategy1, strategy2, normalise = True, return_moves = False):
-	num_iter = 0
+	game_length = roll_game_length()
 	p1_moves = []
 	p2_moves = []
 	p1_payoff = 0
 	p2_payoff = 0
-	while True:
+	for _ in range(game_length):
 		p1_move = play(strategy1, p1_moves, p2_moves)
 		p2_move = play(strategy2, p2_moves, p1_moves)
 		
@@ -35,14 +41,10 @@ def game(strategy1, strategy2, normalise = True, return_moves = False):
 		p2_moves.append(p2_move)
 		p1_payoff += payoff(p1_move, p2_move)
 		p2_payoff += payoff(p2_move, p1_move)
-		num_iter += 1
-		
-		if random.random() >= parameters.delta:
-			break
 	
 	if normalise:
-		p1_payoff /= num_iter
-		p2_payoff /= num_iter
+		p1_payoff /= game_length
+		p2_payoff /= game_length
 	
 	if return_moves:
 		return p1_payoff, p2_payoff, p1_moves, p2_moves
