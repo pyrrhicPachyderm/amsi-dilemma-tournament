3f38f31d32843dcffedb566c3bc7ea72b714b610
Christopher Brown <pyrrhicpachyderm@gmail.com>
2023-07-13 14:14:20 +1000

    Fix a potential bug in get_observation() if it overruns the observation size.

--- utils/rl/observe.py
+++ utils/rl/observe.py
@@ -8,7 +8,11 @@ observation_space_size = math.ceil(math.log(overrun_chance, parameters.delta))
 def get_observation(p1_moves, p2_moves):
 	observation = [2] * (2 * observation_space_size)
 	for i,move in enumerate(p1_moves):
+		if i >= observation_space_size:
+			break
 		observation[i] = int(move)
 	for i,move in enumerate(p1_moves):
+		if i >= observation_space_size:
+			break
 		observation[i + observation_space_size] = int(move)
 	return observation
